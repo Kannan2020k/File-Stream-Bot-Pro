0cafdf7b6a64d82422bacccfa4837f5735255a6f
Kannan2020k <106503465+Kannan2020k@users.noreply.github.com>
2025-08-06 15:31:29 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,37 +9,37 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '11973721'))
-    API_HASH = str(getenv('API_HASH', '5264bf4663e9159565603522f58d3c18'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN','5990559544:AAHUhUFy54tDKGubpU_dMSpSGTRthmgImAw'))
+    API_ID = int(getenv('API_ID', '26872474'))
+    API_HASH = str(getenv('API_HASH', 'f8d3a289bf28a13a7159ad0b2ed114e7'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN','5452246619:6972545265:AAGQt_bfDfc7xOSY268-m7dQHmjUIPjFZKw'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001871766752'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001850979293'))
     PORT = int(getenv('PORT', '8080'))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'starstreamer.hop.sh'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1391556668").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5211097531").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     PORT = int(environ.get("PORT", 8080))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', '@TG_Karthik'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', '@RBEagle2k'))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME', 'file-stream-bot-pro'))
+        APP_NAME = str(getenv('APP_NAME', 'file-stream-bot'))
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', 'starstreamer.hop.sh')) if not ON_HEROKU or getenv('FQDN', '16.171.19.76:8080') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com''
     HAS_SSL=bool(getenv('HAS_SSL', True))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://KarthikMovies:KarthikUK007@cluster0.4l5byki.mongodb.net/?retryWrites=true&w=majority'))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://Karnan2k:karnan2k@cluster0.guq8k77.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Star_Bots_Tamil'))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001296894100")).split())) 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001821439025"))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001850979293")).split())) 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001850979293"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
     KEEP_ALIVE = str(environ.get("KEEP_ALIVE", "1").lower()) in  ("1", "true", "t", "yes", "y")
     DEBUG = str(environ.get("DEBUG", "0").lower()) in ("1", "true", "t", "yes", "y")
